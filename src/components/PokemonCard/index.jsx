ccc705b7128f53ff3ec371cc9697de280f0ef0cc
Paulo Ricardo <pauloricardordk@yahoo.com.br>
2023-05-19 19:45:57 -0300

    Card fixes and new style.

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -4,7 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Chip, CircularProgress } from "@mui/material";
+import { CardActionArea, Chip, CircularProgress, Divider } from "@mui/material";
 import { backgroundCaptalize } from "../../utils/backgroundCaptalize";
 import { typeCaptalize } from "../../utils/typeCaptalize";
 import axios from "axios";
@@ -74,20 +74,18 @@ export default function PokemonCard({ name, url }) {
           ></img>
         </CardMedia>
         <CardContent>
-          <Box
-            display="flex"
-            justifyContent="space-evenly"
-            alignItems="center"
-          >
+          <Box display="flex" justifyContent="space-evenly" alignItems="center">
             <Typography
               sx={{ letterSpacing: 2, fontFamily: "PT Sans Narrow" }}
               gutterBottom
-              variant="h5"
+              variant="h5"          
               component="div"
             >
               {name}
             </Typography>
-
+          </Box>
+          <Divider />
+          <Box sx={{ display: "flex", justifyContent: "space-around", marginTop: 2 }}>
             <ChipType types={pokemon.types} />
           </Box>
         </CardContent>
